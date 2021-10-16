f4ceb8567eec141207e40f8fc8f79287e894d6c9
Ruchi Mandve <pracman07@gmail.com>
2021-10-16 10:43:43 +0530

    [ruchi]: addes eureka server part

---
+++ src/test/java/com/bridgelabz/FundooEurekanew/FundooEurekanewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.FundooEurekanew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooEurekanewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
